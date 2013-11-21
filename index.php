77070b7e6665f3de29e049d685e5b7bc1ffab4a8
Icaro William <dev@mob2you.com.br>
2013-11-21 05:26:29 -0200

    tentando corrigir a home

--- index.php
+++ index.php
@@ -21,6 +21,10 @@ img {
 <div style='position:absolute;top:9px;left:67px;z-index:0;'><img src="img/newHome/logo.png" /></div>
 
 <div style='position:absolute;top:136px;right:10px;z-index:1;'><img src="img/newHome/img_slide_1.png" /></div>
+<!--
+<div style='position:absolute;top:136px;right:10px;z-index:1;'><img src="img/newHome/img_slide_2.png" /></div> 
+
+<div style='position:absolute;top:8px;right:5px;z-index:1;'><img src="img/newHome/img_slide_3.png" /></div> -->
 
 <div style="position:absolute;top:10px;right:95px;z-index:2;text-align:right;">
 	<span>0800 - 703 1533</span><br />
@@ -35,16 +39,50 @@ img {
     <a class=menu href="contato.php">CONTATO</a>
 </div>
 
-<div>
-	<p>Corpo do SITE!!! Tirar seta do MENU</p>
+<!-- CORPO DO SITE-->
+
+<!-- slider 1 -->
+<div style="position:absolute;top:450px;left:477px;">
+	<img src="img/newHome/botao_laranja.png" /><br><br>
+    <img style="position:absolute;top:47px;left:29px;" src="img/newHome/botao_laranja.png" /><br><br>
+    <img style="position:absolute;top:85px;left:67px;" src="img/newHome/botao_azul.png" />
 </div>
 
-              
-    
-<div class="clear"></div>
-</div><br><br> <!-- slider -->
+<div class="textoSlider1" style="position:absolute;top:200px;left:67px;">
+<div id="txtProtecao">
+	<p><span style="font-size: 28px;font-weight: bold;color:#e98f1a;">PROTECAO</span> 
+    <span style="font-size: 28px;color:#e98f1a;">SOB MEDIDA</span><br>
+	<span style="font-size: 45px;color:#898989; letter-spacing:-2px !important;">PARA O MELHOR</span><br>
+	<span style="font-size: 45px;color:#898989;">DO SEU</span> 
+    <span style="font-size: 45px;color:#e98f1a; font-weight: bold;">SONHO</span></p>	
+</div>
 
-<div style='text-align:center; color:#5A5A5A; font-size:15px; background-color:#ffffff; padding:12px; font-weight:bold;position:absolute;top:800px;'>
+<div class="faixaAzul" style="position:absolute;top:165px;left:-32px;">
+	<img src="img/newHome/faixa_azul.png" />
+</div>
+
+<div id="txtCeramica" style="position:absolute;top:179px;left:0;text-align:right;text-align:left;">
+			<span style="font-size: 19px;text-align:left;">A 
+            <span style="color:#ca7500;font-weight:bold;text-align:left;">CERAMICA BARROBELLO,</span>
+             tem por 
+				responsabilidade e compromisso a melhoria
+				continua de seus servicos e produtos, sempre 
+				buscando um alto padrao de qualidade e 
+				pontualidade para satisfacao de seus clientes e 
+				parceiros.</span>
+			</div><!--txtCeramica -->
+<!-- FIM: slider 1 -->
+
+<div style='width:1024px;height:500px; position:absolute;top:462px;left:0; z-index:1;background-image:url(img/newHome/bgMeio.png);background-repeat:repeat-y;'></div>
+<div style='width:1024px;height:500px; position:absolute;top:491px;left:0; z-index:1;background-image:url(img/newHome/img_projetoLeft.png);background-repeat:no-repeat;'></div>
+
+<div style='height:390px;position:absolute;top:955px;left:0px; z-index:2;'><img src="img/newHome/bgBlocosTelhas.png" /></div>
+            
+<!--FIM: CORPO DO SITE -->    
+<div class="clear"></div>
+</div><br><br> 
+
+<div style='text-align:center; color:#5A5A5A; font-size:15px; background-color:#ffffff; padding:12px; font-weight:bold;position:absolute;top:1700px;'>
 	Rodovia Anhanguera, saída 199 Km 01 Santa Cruz da Conceição/SP CEP 13625-000<br>
 	© 2013 - Barrobello Blocos e Telhas Cerâmicas - Direitos Reservados
 </div>
