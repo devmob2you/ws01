1c6f991f267cd1658fe36d63db1848da10afe517
Icaro William <dev@mob2you.com.br>
2013-11-21 03:30:27 -0200

    alterando a home

--- index.php
+++ index.php
@@ -2,56 +2,49 @@
 <html lang="pt-BR">
 <head>
 <title>Barrobello - Blocos e telhas cerâmicas</title>
-<style>
-body{font-family: MyriadPro-Regular, 'Myriad Pro Regular', MyriadPro, 'Myriad Pro', Helvetica, Arial, sans-serif;}
+<style rel="stylesheet" type="text/css">
+*{font-family: MyriadPro-Regular, 'Myriad Pro Regular', MyriadPro, 'Myriad Pro', Helvetica, Arial, sans-serif;}
 .menu{display:inline; line-height:60px; text-decoration:none; color:#9b9b9a; font-weight:bold; padding:0 7px; text-align:center;}
 .menu:hover{color:#f7a565;}
-</style>
-   	<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/skin.css" />
-	<script type="text/javascript">
-	$(document).ready(function () {
-        
-    });
-	
-</script>
 
-<style rel="stylesheet" type="text/css">
-body {
-    font-family:arial;    
-}
 img {
     border:0;
 }
 </style>
 
-
+<script src="js/mob2size.js"></script> <!--REMOVER APÓS TESTES -->
 </head>
-<body style='margin:0px; background-image:url(imagens/bgblocos.png);'>
-<div style='width:980px; margin:0px auto; background-image:url(imagens/bgtopoblocos.png); background-position:right; background-repeat:no-repeat;'>
-	<div style='display:table-cell; width:360px; height:140px; text-align:center;'><a href="index.php"><img src="imagens/logo.png" border=0></a></div>
-	<div style='display:table-cell; width:620px; height:140px; vertical-align:top;'>
-		<div align="right" style='color:#5b5b5b; font-weight:bold; font-size:12px; padding:40px 8px 0px 0px;'>
-			0800 - 703 1533<br />TEL/FAX: 21(19)3567-1533 - vendas@ceramicabarrobello.com.br
-		</div>
-		<div style="padding:0px; margin:20px; margin-right:45px; text-align:right;">
-			<a class=menu href="ceramica.php" style='padding-top:26px; background-image:url(imagens/cursor.png); background-position:center top; background-repeat:no-repeat;'>A CERÂMICA</a>
-			<a class=menu href="blocos.php">BLOCOS</a>
-			<a class=menu href="telhas.php">TELHAS</a>
-			<a class=menu href="fotos.php">FOTOS</a>
-			<a class=menu href="contato.php">CONTATO</a>
-		</div>
-	</div>
-</div><br><br>
+<!--<body style='margin:0px; background-image:url(imagens/bgblocos.png);z-index:0;'>-->
+<body>
+<div style='position:absolute;top:0;right:0;z-index:0;'><img src="img/newHome/bgTopo.png" /></div>
 
-<p>Corpo do SITE!!! Tirar seta do MENU</p>
-                
+<div style='position:absolute;top:9px;left:67px;z-index:0;'><img src="img/newHome/logo.png" /></div>
+
+<div style='position:absolute;top:136px;right:10px;z-index:1;'><img src="img/newHome/img_slide_1.png" /></div>
+
+<div style="position:absolute;top:10px;right:95px;z-index:2;text-align:right;">
+	<span>0800 - 703 1533</span><br />
+    TEL/FAX: 21(19)3567-1533 - vendas@ceramicabarrobello.com.br
+</div>
+
+<div style="position:absolute;top:70px;right:130px;z-index:2">
+	<a class=menu href="ceramica.php">A CERÂMICA</a>
+    <a class=menu href="blocos.php">BLOCOS</a>
+    <a class=menu href="telhas.php">TELHAS</a>
+    <a class=menu href="fotos.php">FOTOS</a>
+    <a class=menu href="contato.php">CONTATO</a>
+</div>
+
+<div>
+	<p>Corpo do SITE!!! Tirar seta do MENU</p>
+</div>
+
+              
     
-    
-    <div class="clear"></div>
+<div class="clear"></div>
 </div><br><br> <!-- slider -->
 
-<div style='text-align:center; color:#5A5A5A; font-size:15px; background-color:#ffffff; padding:12px; font-weight:bold;'>
+<div style='text-align:center; color:#5A5A5A; font-size:15px; background-color:#ffffff; padding:12px; font-weight:bold;position:absolute;top:800px;'>
 	Rodovia Anhanguera, saída 199 Km 01 Santa Cruz da Conceição/SP CEP 13625-000<br>
 	© 2013 - Barrobello Blocos e Telhas Cerâmicas - Direitos Reservados
 </div>
